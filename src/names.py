7bf349432863b617854896c2e2dd2c5243c31376
eldemee <demetrio.lop3zlanzas@gmail.com>
2023-11-17 15:45:09 +0100

    actu

--- src/names.py
+++ src/names.py
@@ -1,17 +1,28 @@
 import csv
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Optional, Set
 
 NameFreq = NamedTuple("NameFreq",[("year",int),("name",str),("frequency", int),("gender",str)])
 
 
 #EXCERCISE 1    
 def read_names_freq(filename:str) -> List[NameFreq]:
-    with open(filename, encoding=True) as file:
+    with open(filename) as file:
         archivo = csv.reader(file)
         next(archivo)
         lista = [NameFreq(int(year),name,int(frequency),gender) for year,name,frequency,gender in archivo]
         return lista
 
 #EXCERCISE 2
+def filter_gender(names: List[NameFreq], g:str) -> List[NameFreq]:
+    if g in ["Hombre", "Mujer"]:
+        return [s for s in names if s.gender == g]
+    
 
+#EXCERCISE 3
+def get_names(names: List[NameFreq], g:Optional[str]=None) -> Set[str]:
+    return {s.name for s in names if g==None or s.gender==g}
+    
+#EXERCISE 4
+def got_top_n_year(names: List[NameFreq], year: int, limit:int=10, g:Optional[str]=None) -> List[Tuple[str,int]]:
+    
 
--- src/names_test.py
+++ src/names_test.py
@@ -5,6 +5,11 @@ def main():
     print(f"we have {len(names_freq)} names frequency")
     print(f"First name and year {names_freq[0]}")
 
+    male_freqs = filter_gender(names_freq, "Hombre")
+    print(f"Hay una frecuencia de {len(male_freqs)} nombres")
+    name_woman = get_names(names_freq, "Mujer")
+    print(f"Hay {len(name_woman)}")
+
 
 if __name__ == "__main__":
     main()
